a60716455ba634bf96685776109113d6faa5d3fc
Philipp <>
2018-05-29 20:06:00 +0200

    start cleanum globals.h

--- globals.h
+++ globals.h
@@ -5,6 +5,8 @@
 
 #include <stdint.h>
 #include <cstdlib>
+#include <vector>
+#include <string>
 
 #define UNDEFINED 0x7FFFFFFF
 #define MAX_MARKERS 200
@@ -19,6 +21,13 @@ enum Orientation {
 struct Marker {
 	int chunkX, chunkZ, offsetX, offsetZ;
 	uint8_t color;
+};
+
+enum WorldFormat {
+	FORMAT1,
+	FORMAT2,
+	FORMAT3,
+	NUM_FORMATS
 };
 
 // Global area of world being rendered
@@ -63,5 +72,44 @@ extern char *g_TilePath;
 extern int8_t g_SectionMin, g_SectionMax;
 
 extern uint8_t g_MystCraftAge;
+
+struct Settings {
+	Orientation orientation; // North, West, South, East
+	bool nightmode;
+	bool underground;
+	bool blendUnderground;
+	bool skylight;
+	int noise;
+	bool blendAll; // If set, do not assume certain blocks (like grass) are always opaque
+	bool hell, serverHell; // rendering the nether
+	bool lowMemory;
+};
+
+class Global
+{
+public:
+	static int TotalFromChunkX, TotalFromChunkZ, TotalToChunkX, TotalToChunkZ; //global area from - to
+	static int FromChunkX, FromChunkZ, ToChunkX, ToChunkZ; // Current area of world being rendered
+	static size_t MapsizeZ, MapsizeX, Terrainsize; // size of that area in blocks (no offset)
+	static int MapminY, MapsizeY; //no idea
+	static int OffsetY; // y pixel offset in the final image for one y step in 3d array (2 or 3)
+	static WorldFormat wolrdFormat;
+	static Settings settings; //Used settings
+
+	// For rendering biome colors properly, external png files are used
+	static bool useBiomes;
+	static uint64_t biomeMapSize;
+	static std::vector<uint8_t> grasscolor, leafcolor, tallGrasscolor;
+	static std::vector<uint16_t> biomeMap;
+	static int grasscolorDepth, foliageDepth;
+
+	static std::vector<Marker> markers;
+	static std::vector<uint8_t> terrain, light; // 3D arrays holding terrain/lightmap
+	static std::vector<uint16_t> heightMap; // 2D array to store min and max block height per X/Z - it's 2 bytes per index, upper for highest, lower for lowest (don't ask!)
+
+	static std::string tilePath; // If output is to be split up (for google maps etc) this contains the path to output to, "" otherwise
+	static int8_t g_SectionMin, g_SectionMax;
+	uint8_t g_MystCraftAge;
+};
 
 #endif
