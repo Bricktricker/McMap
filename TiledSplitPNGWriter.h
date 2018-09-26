15a2373c7c148e9cf2b04c34dd89683d5b184709
Philipp <>
2018-09-26 13:26:50 +0200

    TiledSplitPNGWriter finished

--- TiledSplitPNGWriter.h
+++ TiledSplitPNGWriter.h
@@ -7,5 +7,17 @@ class TiledSplitPNGWriter : public TiledPNGWriter
 public:
 	TiledSplitPNGWriter(const size_t origW, const size_t origH);
 	bool compose(const std::string& path) override;
+private:
+
+	struct ImageTile {
+		std::fstream fileHandle;
+		png_structp pngPtr;
+		png_infop pngInfo;
+
+		ImageTile()
+			: pngPtr(nullptr), pngInfo(nullptr) {}
+
+	};
+
 };
 
